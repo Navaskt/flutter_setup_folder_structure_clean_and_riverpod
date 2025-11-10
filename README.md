6ca1c03046d643c1872fc143ced76ec6cc61f795
Mohammed Navas <navaskt38@gmail.com>
2025-11-10 11:40:09 +0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# flutter_setup_folder_structure_clean_and_riverpod
+Project folder auto set up for clean and riverpod
---
+++ android/app/src/main/kotlin/com/example/flutter_setup_folder_structure_clean_and_riverpod/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_setup_folder_structure_clean_and_riverpod
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
